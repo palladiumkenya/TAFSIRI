2c03f9ee533b3651a15dc81eb445a421d42da0ab
NewtonMutugi <newtonmut197@gmail.com>
2024-07-17 10:38:42 +0300

    chore: Initialized FastAPI application with CORS middleware and health checker endpoint

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes import access_api, dictionary_mapper_api, data_dictionary_api, text2sql_api
+
+app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(text2sql_api.router, tags=[
+                   'Text2SQL'], prefix='/api/text2sql')
+
+
+@app.get("/api/healthchecker")
+def root():
+    return {"message": "Welcome to data map, we are up and running"}
+
+
+# Run the FastAPI application
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
