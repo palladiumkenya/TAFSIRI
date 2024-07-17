b4678f2043349eecd27392a3d44c5a6219673492
NewtonMutugi <newtonmut197@gmail.com>
2024-07-17 10:39:22 +0300

    chore: Add settings module with environment variables

---
+++ settings.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # CASSANDRA_HOST: str
+    # CASSANDRA_PORT: int
+    # CASSANDRA_DB: str
+    # CASSANDRA_USER: str
+    # CASSANDRA_PASSWORD: str
+
+    REPORTING_DB: str
+    REPORTING_USER: str
+    REPORTING_PASSWORD: str
+    REPORTING_HOST: str
+    OPENAI_KEY: str
+
+    OM_HOST: str
+    OM_JWT: str
+
+    class Config:
+        env_file = './.env'
+        extra = 'ignore'
+
+
+settings = Settings()
